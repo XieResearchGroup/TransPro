5563619f9caa8854833c4e9674163434c302ec25
Yoyo Wu <60115432+AdorableYoyo@users.noreply.github.com>
2023-03-04 23:58:56 -0500

    Update Ic_50_task.sh

--- scripts/Ic_50_task.sh
+++ scripts/Ic_50_task.sh
@@ -1,7 +1,7 @@
 
-for split in rand_0 #rand_1 rand_2 rand_3
+for split in rand_0 rand_1 rand_2 rand_3
 do
-for lr in 0.00005 #0.00002 0.0001  0.00005 
+for lr in 0.00005 0.00002 0.0001 
 do
 for dop in 0.2
 do
@@ -11,7 +11,7 @@ seed=343
 exp_id=${job}_emb_${split}_bs_${batch_size}_dop_${dop}_lr_${lr}_sd_${seed}
 python transPro_Ic50_ft.py \
 --exp_id ${exp_id} --dop ${dop} --seed ${seed} --batch_size ${batch_size}  \
---device 5 --max_epochs 5 --lr ${lr} --job ${job} \
+--device 5 --max_epochs 1000 --lr ${lr} --job ${job} \
 --Ic_50_train_dir "data/Ic50/${split}_train_ic50_wo_na.csv" \
 --Ic_50_dev_dir "data/Ic50/${split}_dev_ic50_wo_na.csv" \
 --Ic_50_test_dir "data/Ic50/rand_0123_test_ic50_wo_na.csv" \
